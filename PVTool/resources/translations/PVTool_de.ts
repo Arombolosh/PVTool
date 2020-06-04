c11f3a79eae119c4b59c93448157f273b90d9cc6
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-06-04 20:02:58 +0200

    Created translation file.

--- PVTool/resources/translations/PVTool_de.ts
+++ PVTool/resources/translations/PVTool_de.ts
@@ -2,9 +2,213 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
+    <name>PVTButtonBar</name>
+    <message>
+        <source>Shows information about the program.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a new default project (Ctrl + N).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loads a project (Ctrl + O).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saves current project (Ctrl + S).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Closes the program (asks for saving confirmation first).</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>PVToolWidget</name>
     <message>
-        <source>Form</source>
+        <source>PVTool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PCM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insulation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>spez. Wärmekapazität in J/kgK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dicke in cm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wärmeleitfähigkeit in W/mK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dichte in kg/m3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Run Simulation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Weather</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Weather File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EPW-File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ausführungsverzeichnis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PV-Module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nennspannung (MPP) in V</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Kurzschlussstrom in A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nennstromstärke (MPP) in A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Leerlaufspannung in V</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PV Herstellerdaten</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PV Datenbank</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zellanzahl</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temperaturkoeffizient Uoc in %/K</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temperaturkoeffizient Isc in %/K</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temperaturkoeffizient PMPP in %/K</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SP26</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SP30</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Öffne Wetterdatei</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EnergyPlus Weather Files (*.epw)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Die ausgewählte Klimadatei &apos;%1&apos; existiert nicht.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Kann Zielklimaverzeichnis &apos;%1&apos; nicht erstellen.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Konnte Klimadatei &apos;%2&apos; nicht ins Arbeitsverzeichnis &apos;%1&apos; kopieren.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bei der Berechnung der PV-Panel-Kenngrößen aus den Eingabedaten ist ein Fehler aufgetreten.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Kann CmdDiscretise-Tool nicht in &apos;%1&apos; finden.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Arbeitsverzeichnis existiert nicht, soll es erstellt werden?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Konnte das Arbeitsverzeichnis nicht erstellen. Möglicherweise fehlen die Zugriffsrechte?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Konnte Projektverzeichnis nicht ins Arbeitsverzeichnis &apos;%1&apos; kopieren.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fehler bei der Ausführung des CmdDiscretize-Tools.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Simuliere Geometrievarianten...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Abbrechen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Setze Ausführungsordner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fehler bei der Ausführung der DELPHIN-Simulation, siehe Logdatei &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <source>Ungültige Eingabe &apos;%1&apos; im Feld &apos;%2&apos;
+Gleitkommazahl wird benötigt.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ungültige Eingabe &apos;%1&apos; im Feld &apos;%2&apos;
+Ganzzahliger Wert wird benötigt.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
