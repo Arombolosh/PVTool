8e8c1c0c13d5c43efb8f0f88f6926060f313c775
Dirk <d-weiss.mail@gmx.de>
2020-09-03 07:57:02 +0200

    version number increased

--- PVTool/src/PVTConstants.cpp
+++ PVTool/src/PVTConstants.cpp
@@ -1,10 +1,10 @@
 #include "PVTConstants.h"
 
-const char * const VERSION = "0.1";
+const char * const VERSION = "0.2";
 
-const char * const LONG_VERSION = "0.1.0";
+const char * const LONG_VERSION = "0.2.0";
 
-const char * const PROGRAM_NAME = "PVTool 0.1";
+const char * const PROGRAM_NAME = "PVTool 0.2";
 
 const char * const ORG_NAME				= "IBK";
 const char * const DOT_FILE_EXTENSION	= ".pv";
