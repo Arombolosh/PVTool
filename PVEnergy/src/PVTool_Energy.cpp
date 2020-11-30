8304b8df364be3a30e4018e48059891e63cb7563
Katja <katja.tribulowski@tu-dresden.de>
2020-11-30 16:42:31 +0100

    add outputs

--- PVEnergy/src/PVTool_Energy.cpp
+++ PVEnergy/src/PVTool_Energy.cpp
@@ -3,6 +3,9 @@
 #include <IBK_Exception.h>
 #include <IBK_messages.h>
 #include <iostream>
+#include <algorithm>
+#include <fstream>
+
 
 namespace PVTOOL {
 
@@ -84,7 +87,98 @@ double Energy::calcPVEnergy(double absTemp, double rad, double airMass) const {
 
 	//Vorberechnung
 
+	//Achtung neue Auswertung
+	if(true){
+		struct pvVals{
+			pvVals(double i, double u) :
+				i(i),
+				u(u),
+				p(i*u)
+			{}
 
+			double i, u, p;
+		};
+
+		std::vector<pvVals>				refValues;
+		std::vector<pvVals>				tempBasedValues;
+
+		volt = 0;
+		while(true){
+			for(size_t i= 0; i<1000; ++i)
+			{
+				double i0 = current;
+				//		double tempVal1 = volt + current * m_pvData.m_rS;
+				//		double tempVal2 = (std::exp((volt + current * m_pvData.m_rS)/m_pvData.m_a)-1);
+				//		double tempVal3 = (volt + current * m_pvData.m_rS) / rSh;
+				//		double tempVal4 = iO * (std::exp((volt + current * m_pvData.m_rS)/m_pvData.m_a)-1);
+				current = iL - iO * (std::exp((volt + current * m_pvData.m_rS)/m_pvData.m_a)-1) - (volt + current * m_pvData.m_rS) / rSh;
+				if(std::fabs(i0-current)<eps)
+					break;
+				if (current < 0 ){
+					//currentIsNegative = true;
+					break;
+				}
+			}
+			if(current <0)
+				break;
+			tempBasedValues.push_back(pvVals(current, volt));
+
+			volt +=0.1;
+		}
+
+		{
+			double iL = m_pvData.m_iLRef;
+			double eg = m_egRef;
+			double rSh = m_pvData.m_rShRef;
+			double iO = m_pvData.m_iORef;
+
+			volt = 0;
+			while(true){
+				for(size_t i= 0; i<1000; ++i)
+				{
+					double i0 = current;
+					current = iL - iO * (std::exp((volt + current * m_pvData.m_rS)/m_pvData.m_a)-1) - (volt + current * m_pvData.m_rS) / rSh;
+					if(std::fabs(i0-current)<eps)
+						break;
+					if (current < 0 ){
+						//currentIsNegative = true;
+						break;
+					}
+				}
+				if(current <0)
+					break;
+				refValues.push_back(pvVals(current, volt));
+
+				volt +=0.1;
+			}
+		}
+		std::ofstream out;
+		out.open("c:/temp/pvPostProc.csv");
+
+		out << "Time [min]/tIref [A]/tUref [V]/tPref [W]/tI [A]/tU [V]/tP [W]" << std::endl;
+		unsigned int sizeRef, size2;
+		sizeRef = refValues.size();
+		size2 = tempBasedValues.size();
+		double time =0;
+		for(unsigned int i=0; i<std::max<unsigned int>(sizeRef, size2); ++i){
+			out << time << "/t";
+			if(i < sizeRef){
+				out << refValues[i].i << "/t";
+				out << refValues[i].u << "/t";
+				out << refValues[i].p << "/t";
+			}
+			else
+				out << "0/t0/t0/t";
+			if(i<size2){
+				out << tempBasedValues[i].i << "/t";
+				out << tempBasedValues[i].u << "/t";
+				out << tempBasedValues[i].p << "/t";
+			}
+			else
+				out << "0/t0/t0/t";
+			out << std::endl;
+		}
+	}
 
 
 	// calculate current and power at MPP --> powerA
