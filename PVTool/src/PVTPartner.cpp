7972d7cf2c4d3ce5643c5ae73177796fb32a3323
Arombolosh <d-weiss.mail@gmx.de>
2021-06-16 09:52:14 +0200

    added partner infos

--- PVTool/src/PVTPartner.cpp
+++ PVTool/src/PVTPartner.cpp
@@ -22,10 +22,10 @@ PVTPartner::PVTPartner(QWidget *parent) :
 	m_ui->plainTextEditADOMetall->setFont(f);
 
 	QString str = "TU Dresden Institut für Bauklimatik\nAnsprechpartner Dirk Weiß\nTel.: +49 351 463 35367\nEmail: dirk.weiss@tu-dresden.de\nWebseite: www.tu-dresden.de/bauklimatik\nwww.bauklimatik-dresden.de";
-	str += "\n\nTU Dresden Institut für Baukonstruktion\nAnsprechpartner Christian Popp\nTel.: +49 351 463 32306\nEmail: christian.popp@tu-dresden.de\nWebseite: YYY";
+	str += "\n\nTU Dresden Institut für Baukonstruktion\nAnsprechpartner Christian Popp\nTel.: +49 351 463 32306\nEmail: christian.popp@tu-dresden.de\nWebseite: www.tu-dresden.de/bu/bauingenieurwesen/bauko";
 
 	m_ui->plainTextEditTUD->appendPlainText(str);
-	str = "PAZDERA AG\nAnsprechpartner: XXX\nHinterer Floßanger 14\nD-96450 Coburg\nTel.: +49 (0)9561 516-0\nEmail: YYYY@yyy.de\nWebseite: www.pazdera.de";
+	str = "PAZDERA AG\nAnsprechpartner: Axel Peschek\nHinterer Floßanger 14\nD-96450 Coburg\nTel.: +49 (0)9561 516-0\nEmail: APeschek@pazdera.de\nWebseite: www.pazdera.de";
 	m_ui->plainTextEditPazdera->appendPlainText(str);
 	str = "Rubitherm Technologies GmbH\nAnsprechpartner: Andreas Lärz\nImhoffweg 6\n12307 Berlin\nTel.:  +49 (30) 7109622-0\nEmail: andreas.laerz@rubitherm.com\nWebseite: www.rubitherm.eu";
 	m_ui->plainTextEditRubitherm->appendPlainText(str);
